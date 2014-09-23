1fb938ad0231806d84e9af88db26ab6047a2230b
Schmook <eschmidt@vdesign.com>
2014-09-23 16:37:39 -0500

    conditional header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -48,31 +48,30 @@
 
       <?php print render($page['header']); ?>
 
-        <?php if ($main_menu): ?>
           <div id="navigation">
-              <nav id="main-menu" role="navigation" tabindex="-1">
-                <?php
-                // This code snippet is hard to modify. We recommend turning off the
-                // "Main menu" on your sub-theme's settings form, deleting this PHP
-                // code block, and, instead, using the "Menu block" module.
-                // @see https://drupal.org/project/menu_block
-                print theme('links__system_main_menu', array(
-                  'links' => $main_menu,
-                  'attributes' => array(
-                    'class' => array('links', 'inline', 'clearfix'),
-                  ),
-                  'heading' => array(
-                    'text' => t('Main menu'),
-                    'level' => 'h2',
-                    'class' => array('element-invisible'),
-                  ),
-                )); ?>
-              </nav>
+            <?php if ($main_menu): ?>
+                  <nav id="main-menu" role="navigation" tabindex="-1">
+                    <?php
+                    // This code snippet is hard to modify. We recommend turning off the
+                    // "Main menu" on your sub-theme's settings form, deleting this PHP
+                    // code block, and, instead, using the "Menu block" module.
+                    // @see https://drupal.org/project/menu_block
+                    print theme('links__system_main_menu', array(
+                      'links' => $main_menu,
+                      'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                      ),
+                      'heading' => array(
+                        'text' => t('Main menu'),
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                      ),
+                    )); ?>
+                  </nav>
 
-            <?php print render($page['navigation']); ?>
-
+                <?php print render($page['navigation']); ?>
+            <?php endif; ?>
           </div>
-        <?php endif; ?>
 
     </header>
   <?php endif; ?>
