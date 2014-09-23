9c6785c2e85854b133ad7939e30591e8c52327aa
Schmook <eschmidt@vdesign.com>
2014-09-23 16:31:32 -0500

    conditional header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -9,36 +9,61 @@
 ?>
 
 <div id="page">
-  <?php if ($header): ?>
-    <header class="header" id="header" role="banner">
 
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-      <?php endif; ?>
+  <header class="header" id="header" role="banner">
 
-      <?php if ($site_name || $site_slogan): ?>
-        <div class="header__name-and-slogan" id="name-and-slogan">
-          <?php if ($site_name): ?>
-            <h1 class="header__site-name" id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    <?php endif; ?>
 
-          <?php if ($site_slogan): ?>
-            <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div>
-      <?php endif; ?>
+    <?php if ($site_name || $site_slogan): ?>
+      <div class="header__name-and-slogan" id="name-and-slogan">
+        <?php if ($site_name): ?>
+          <h1 class="header__site-name" id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
 
-      <?php if ($secondary_menu): ?>
-        <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-          <?php print theme('links__system_secondary_menu', array(
-            'links' => $secondary_menu,
+        <?php if ($site_slogan): ?>
+          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($secondary_menu): ?>
+      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+        <?php print theme('links__system_secondary_menu', array(
+          'links' => $secondary_menu,
+          'attributes' => array(
+            'class' => array('links', 'inline', 'clearfix'),
+          ),
+          'heading' => array(
+            'text' => $secondary_menu_heading,
+            'level' => 'h2',
+            'class' => array('element-invisible'),
+          ),
+        )); ?>
+      </nav>
+    <?php endif; ?>
+
+    <?php print render($page['header']); ?>
+
+    <div id="navigation">
+
+      <?php if ($main_menu): ?>
+        <nav id="main-menu" role="navigation" tabindex="-1">
+          <?php
+          // This code snippet is hard to modify. We recommend turning off the
+          // "Main menu" on your sub-theme's settings form, deleting this PHP
+          // code block, and, instead, using the "Menu block" module.
+          // @see https://drupal.org/project/menu_block
+          print theme('links__system_main_menu', array(
+            'links' => $main_menu,
             'attributes' => array(
               'class' => array('links', 'inline', 'clearfix'),
             ),
             'heading' => array(
-              'text' => $secondary_menu_heading,
+              'text' => t('Main menu'),
               'level' => 'h2',
               'class' => array('element-invisible'),
             ),
@@ -46,37 +71,11 @@
         </nav>
       <?php endif; ?>
 
-      <?php print render($page['header']); ?>
+      <?php print render($page['navigation']); ?>
 
-      <div id="navigation">
+    </div>
 
-        <?php if ($main_menu): ?>
-          <nav id="main-menu" role="navigation" tabindex="-1">
-            <?php
-            // This code snippet is hard to modify. We recommend turning off the
-            // "Main menu" on your sub-theme's settings form, deleting this PHP
-            // code block, and, instead, using the "Menu block" module.
-            // @see https://drupal.org/project/menu_block
-            print theme('links__system_main_menu', array(
-              'links' => $main_menu,
-              'attributes' => array(
-                'class' => array('links', 'inline', 'clearfix'),
-              ),
-              'heading' => array(
-                'text' => t('Main menu'),
-                'level' => 'h2',
-                'class' => array('element-invisible'),
-              ),
-            )); ?>
-          </nav>
-        <?php endif; ?>
-
-        <?php print render($page['navigation']); ?>
-
-      </div>
-
-    </header>
-  <?php endif; ?>
+  </header>
 
   <div id="main">
 
