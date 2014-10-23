458c8342a037ef292785f7a7f1a1141ec7272c53
Schmook <eschmidt@vdesign.com>
2014-10-23 17:39:36 -0500

    fullpage options

--- js/script.js
+++ js/script.js
@@ -48,55 +48,6 @@ Drupal.behaviors.my_custom_behavior = {
     };
 
     //fullPage.js
-    $('#fullpage').fullpage({
-            //Navigation
-            menu: true,
-            anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10'],
-            navigation: true,
-            navigationPosition: 'right',
-            navigationTooltips: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10'],
-            slidesNavigation: true,
-            slidesNavPosition: 'bottom',
-
-            //Scrolling
-            css3: true,
-            scrollingSpeed: 700,
-            autoScrolling: true,
-            easing: 'easeInQuart',
-            easingcss3: 'ease',
-            loopBottom: false,
-            loopTop: false,
-            loopHorizontal: true,
-            continuousVertical: false,
-            normalScrollElements: '#element1, .element2',
-            scrollOverflow: false,
-            touchSensitivity: 15,
-            normalScrollElementTouchThreshold: 5,
-
-            //Accessibility
-            keyboardScrolling: true,
-            animateAnchor: true,
-
-            //Design
-            verticalCentered: true,
-            resize : true,
-            sectionsColor : ['#ccc', '#fff'],
-            paddingTop: '3em',
-            paddingBottom: '10px',
-            fixedElements: '#header, .footer',
-            responsive: 0,
-
-            //Custom selectors
-            sectionSelector: '.section',
-            slideSelector: '.slide',
-
-            //events
-            onLeave: function(index, nextIndex, direction){},
-            afterLoad: function(anchorLink, index){},
-            afterRender: function(){},
-            afterResize: function(){},
-            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-            onSlideLeave: function(anchorLink, index, slideIndex, direction){}
-        });
+    $('#fullpage').fullpage();
 
 })(jQuery, Drupal, this, this.document);
