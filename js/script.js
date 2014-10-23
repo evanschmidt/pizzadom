faf8d6034468c9bc75d658bdf4d5ddc7ee3e7862
Schmook <eschmidt@vdesign.com>
2014-10-23 17:47:02 -0500

    fullpage options

--- js/script.js
+++ js/script.js
@@ -20,7 +20,56 @@ Drupal.behaviors.my_custom_behavior = {
     // Place your code here.
 
     //fullPage.js
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['firstSlide', 'secondSlide'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: false,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+
+        //Design
+        verticalCentered: true,
+        resize : true,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsive: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
 
     //Configure colorbox call back to resize with custom dimensions
       $.colorbox.settings.onLoad = function() {
