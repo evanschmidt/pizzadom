6387b3c64d6985f3d4b1a59bbfbac609d5dfc3f5
Schmook <eschmidt@vdesign.com>
2014-10-23 14:54:28 -0500

    adding new region

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -96,7 +96,7 @@
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      <?php print render($page['content']); ?>
+      <?php print render($page['content_fullpage']); ?>
       <?php print $feed_icons; ?>
     </div>
 
