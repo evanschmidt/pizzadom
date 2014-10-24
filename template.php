6c4e4d686f985a50f513a9662a70e789466e48bd
Schmook <eschmidt@vdesign.com>
2014-10-24 17:13:14 -0500

    playing with menu links

--- template.php
+++ template.php
@@ -138,3 +138,19 @@ function pizzadom_preprocess_block(&$variables, $hook) {
   //}
 }
 // */
+function pizzadom_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+ 
+ // This is where you check the menu name and assign the class.
+  if($menuname == 'main-menu') {
+    $element['#localized_options']['attributes']['data-anchor'][] = 'section-';
+  }
+
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
