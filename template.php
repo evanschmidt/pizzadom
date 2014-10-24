951f8bc2007db808e11ce21392840a2abd789984
Schmook <eschmidt@vdesign.com>
2014-10-24 10:43:22 -0500

    moving jquery load to preprocess_page

--- template.php
+++ template.php
@@ -36,13 +36,9 @@ function pizzadom_preprocess_maintenance_page(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("html" in this case.)
  */
+/* -- Delete this line if you want to use this function
 function pizzadom_preprocess_html(&$variables, $hook) {
-  //$variables['sample_variable'] = t('Lorem ipsum.');
-
-  drupal_add_css(libraries_get_path('jquery.fullpage') . '/jquery.fullPage.css');
-  drupal_add_js(libraries_get_path('jquery.fullpage') . '/vendors/jquery.slimscroll.min.js');
-  drupal_add_js(libraries_get_path('jquery.fullpage') . '/vendors/jquery.easings.min.js');
-  drupal_add_js(libraries_get_path('jquery.fullpage') . '/jquery.fullPage.js');
+  $variables['sample_variable'] = t('Lorem ipsum.');
 
   // The body tag's classes are controlled by the $classes_array variable. To
   // remove a class from $classes_array, use array_diff().
@@ -58,9 +54,16 @@ function pizzadom_preprocess_html(&$variables, $hook) {
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
-/* -- Delete this line if you want to use this function
 function pizzadom_preprocess_page(&$variables, $hook) {
-  $variables['sample_variable'] = t('Lorem ipsum.');
+  //$variables['sample_variable'] = t('Lorem ipsum.');
+
+  if ($vars['is_front']) {
+    drupal_add_css(libraries_get_path('jquery.fullpage') . '/jquery.fullPage.css');
+    drupal_add_js(libraries_get_path('jquery.fullpage') . '/vendors/jquery.slimscroll.min.js');
+    drupal_add_js(libraries_get_path('jquery.fullpage') . '/vendors/jquery.easings.min.js');
+    drupal_add_js(libraries_get_path('jquery.fullpage') . '/jquery.fullPage.js');
+  }
+
 }
 // */
 
