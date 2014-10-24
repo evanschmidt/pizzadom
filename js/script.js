12899ee25c1790e446ce0901dd9eb739869ec39a
Schmook <eschmidt@vdesign.com>
2014-10-24 17:06:06 -0500

    fullpage slider

--- js/script.js
+++ js/script.js
@@ -21,10 +21,6 @@ Drupal.behaviors.my_custom_behavior = {
 
     //fullPage.js
     $('#fullpage').fullpage({
-        //Navigation
-        menu: #block-system-main-menu .menu,
-        anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10'],
-        navigationTooltips: ['section1', 'hi-neighbor', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10'],
     });
  /*
     $('#fullpage').fullpage({
