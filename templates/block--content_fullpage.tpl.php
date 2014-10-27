b8a1607ea1a5bbcafda2f496ee88471d90aec86a
Schmook <eschmidt@vdesign.com>
2014-10-27 15:38:49 -0500

    block title hr

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -12,7 +12,9 @@
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
   <?php	$title = str_replace("-", " ", $title); ?>
+  	<hr>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  	<hr>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
   <?php print $content; ?>
