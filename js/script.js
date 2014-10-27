a62345831fb9f5c36caf1d4b4ea6796e4d53699e
Schmook <eschmidt@vdesign.com>
2014-10-27 12:28:52 -0500

    fullpage navigation tips

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ Drupal.behaviors.my_custom_behavior = {
         scrollingSpeed: 1700,
         autoScrolling: false,
         resize : false,
+        navigationTooltips: ['section1', 'section2', 'section3', 'section4', 'section5', 'how-we-roll', 'section7', 'section8', 'section9', 'section10'],
     });
  /*
     $('#fullpage').fullpage({
