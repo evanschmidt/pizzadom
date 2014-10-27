81a6e68985d481518edbb196d793bcd0391c2a78
Schmook <eschmidt@vdesign.com>
2014-10-27 12:40:01 -0500

    fullpage anchors

--- templates/block--content_fullpage.tpl.php
+++ templates/block--content_fullpage.tpl.php
@@ -7,14 +7,17 @@
  * @see https://drupal.org/node/1728246
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="section section-<?php print $block_id; ?> <?php print $block_html_title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
+<div id="<?php print $block_html_id; ?>" class="section section-<?php print $block_id; ?> <?php print $title; ?> <?php print $classes; ?>"<?php print $attributes; ?> data-anchor="section<?php print $block_id; ?>" >
 
+<?php 
+/*
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
     <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
+*/
+?>
   <?php print $content; ?>
 
 </div>
