90874c3a2f3765babfdb351b08214488100b68e7
Schmook <eschmidt@vdesign.com>
2014-10-27 10:53:16 -0500

    menu tweaks

--- template.php
+++ template.php
@@ -163,11 +163,8 @@ function pizzadom_menu_link(array $variables) {
   $element['#attributes']['data-menuanchor'][] = 'menu-' . $element['#original_link']['mlid'] . ' '.$name_id;
 */
   if (isset($element['#localized_options']['fragment'])) {
-    //$element['#attributes']['data-menuanchor'][] = $element['#localized_options']['fragment'];
+    $element['#attributes']['data-menuanchor'][] = $element['#localized_options']['fragment'];
   }
-  if (isset($element['#localized_options']['fragment'])) {
-   dpm($element['#localized_options']['fragment']);
-  }  
 
   $output = l($element['#title'], $element['#href'], $element['#localized_options']);
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
