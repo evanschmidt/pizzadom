5b4bb43898cd69080fdbce69142137124bc61efc
Schmook <eschmidt@vdesign.com>
2014-10-28 09:50:15 -0500

    fullpage scrolling speed

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ Drupal.behaviors.my_custom_behavior = {
 
     //fullPage.js
     $('#fullpage').fullpage({
-        scrollingSpeed: 100,
+        scrollingSpeed: 400,
         autoScrolling: false,
         anchors:['welcome', 'hi-neighbor', 'instagram', 'our-story', 'our-team', 'how-we-roll', 'our-videos', 'our-farmers', 'our-contact', 'our-menu'],
         navigationTooltips: ['welcome', 'hi-neighbor', 'instagram', 'our-story', 'our-team', 'how-we-roll', 'our-videos', 'our-farmers', 'our-contact', 'our-menu'],
