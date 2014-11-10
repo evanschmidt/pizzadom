0e20085ffe4a847c7ebf5be6ccb453ebfe84fe34
Schmook <eschmidt@vdesign.com>
2014-11-10 13:06:18 -0600

    colorbox close set our-story p width

--- js/script.js
+++ js/script.js
@@ -96,7 +96,7 @@ Drupal.behaviors.my_custom_behavior = {
 
     //After colorbox closes
     $(document).bind('cbox_closed', function(){
-        $('.our-story p').css('width','100%');
+        $('.our-story p').css('width','1200px');
         alert("colorbox closed");
     });
 
